4c5d801dd296f8d5208df5f6be8ee501866d7c35
LukasSeiler <lukas.seiler@gibmit.ch>
2020-12-30 18:22:25 +0100

    Kommentierung meines Codes abgeschlossen

--- src/GUI4.java
+++ src/GUI4.java
@@ -125,36 +125,43 @@ public class GUI4 extends JFrame{
 		JTstrasse.setBounds(10, 258, 179, 33);
 		panel.add(JTstrasse);
 		
+		/*Label für die Beschriftung der PLZ*/
 		JLabel JLtitleplz = new JLabel("PLZ:");
 		JLtitleplz.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLtitleplz.setBounds(226, 227, 78, 33);
 		panel.add(JLtitleplz);
 		
+		/*TextField für die PLZ*/
 		JTplz = new JTextField();
 		JTplz.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JTplz.setBounds(226, 258, 179, 33);
 		panel.add(JTplz);
 		
+		/*Label für die Beschriftung des Orts*/
 		JLabel JLtitleort = new JLabel("Ort:");
 		JLtitleort.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLtitleort.setBounds(10, 302, 78, 33);
 		panel.add(JLtitleort);
 		
+		/*TextField für den Ort*/
 		JTort = new JTextField();
 		JTort.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JTort.setBounds(10, 333, 179, 33);
 		panel.add(JTort);
 		
+		/*Label für die Beschriftung des Landes*/
 		JLabel JLtitleland = new JLabel("Land:");
 		JLtitleland.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLtitleland.setBounds(226, 302, 78, 33);
 		panel.add(JLtitleland);
 		
+		/*TextField für das Land*/
 		JTextField JTland = new JTextField();
 		JTland.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JTland.setBounds(226, 333, 179, 33);
 		panel.add(JTland);
 		
+		/*Button Weiter zum Verlasssen der Frame*/
 		JLabel JLweiter = new JLabel("Flug buchen >");
 		JLweiter.setBackground(myOrange);
 		JLweiter.setOpaque(true);
@@ -164,6 +171,7 @@ public class GUI4 extends JFrame{
 		JLweiter.setBounds(10, 390, 179, 49);
 		panel.add(JLweiter);
 		
+		/*Button Zurück zum Wechseln in die letzte Frame*/
 		JLabel JLzurück = new JLabel("Zurück <");
 		JLzurück.setBackground(Color.LIGHT_GRAY);
 		JLzurück.setOpaque(true);
@@ -172,41 +180,52 @@ public class GUI4 extends JFrame{
 		JLzurück.setBounds(226, 390, 179, 49);
 		panel.add(JLzurück);
 		
+		/*Label für das Obligatorisch-Zeichen*/
 		JLabel o1 = new JLabel("*");
 		o1.setForeground(Color.RED);
 		o1.setBounds(62, 11, 33, 21);
 		panel.add(o1);
 		
+		/*Label für das Obligatorisch-Zeichen*/
 		JLabel o2 = new JLabel("*");
 		o2.setForeground(Color.RED);
 		o2.setBounds(73, 89, 33, 21);
 		panel.add(o2);
 		
+		/*Label für das Obligatorisch-Zeichen*/
 		JLabel o3 = new JLabel("*");
 		o3.setForeground(Color.RED);
 		o3.setBounds(295, 88, 33, 21);
 		panel.add(o3);
 		
+		/*Label für das Obligatorisch-Zeichen*/
 		JLabel o4 = new JLabel("*");
 		o4.setForeground(Color.RED);
 		o4.setBounds(62, 227, 33, 21);
 		panel.add(o4);
 		
+		/*Label für das Obligatorisch-Zeichen*/
 		JLabel o5 = new JLabel("*");
 		o5.setForeground(Color.RED);
 		o5.setBounds(253, 226, 33, 21);
 		panel.add(o5);
 		
+		/*Label für das Obligatorisch-Zeichen*/
 		JLabel o6 = new JLabel("*");
 		o6.setForeground(Color.RED);
 		o6.setBounds(38, 302, 33, 21);
 		panel.add(o6);
 		
+		/*Label für das Obligatorisch-Zeichen*/
 		JLabel o7 = new JLabel("*");
 		o7.setForeground(Color.RED);
 		o7.setBounds(265, 302, 22, 21);
 		panel.add(o7);
 		
+		/**
+		 * Onclick-Action für Weiter:
+		 * Es werden diverse Checks gemacht,welche unten aufgeführt sind
+		 */
 		JLweiter.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -217,57 +236,73 @@ public class GUI4 extends JFrame{
 				boolean JTortstatus = false;
 				boolean JTlandstatus = false;
 				
+				/*Es wird geprpft, ob der Vorname definiert wurde*/
 				if(! JTvorname.getText().isEmpty() && JTvorname.getText() != null) {
 					JTvorname.setBackground(Color.white);
 					JTvornamestatus = true;
 				}else {
+					/*Hintergrund des TextFields wird gelb*/
 					JTvorname.setBackground(Color.yellow);
 					JTvorname.setOpaque(true);
 				}
 				
+				/*Es wird geprpft, ob der Nachname definiert wurde*/
 				if(! JTnachname.getText().isEmpty() && JTnachname.getText() != null) {
 					JTnachname.setBackground(Color.white);
 					JTnachnamestatus = true;
 				}else {
+					/*Hintergrund des TextFields wird gelb*/
 					JTnachname.setBackground(Color.yellow);
 					JTnachname.setOpaque(true);
 				}
 				
+				/*Es wird geprpft, ob die Strasse definiert wurde*/
 				if(! JTstrasse.getText().isEmpty() && JTstrasse.getText() != null) {
 					JTstrasse.setBackground(Color.white);
 					JTstrassestatus = true;
 				}else {
+					/*Hintergrund des TextFields wird gelb*/
 					JTstrasse.setBackground(Color.yellow);
 					JTstrasse.setOpaque(true);
 				}
 				
+				/*Es wird geprpft, ob die PLZ definiert wurde*/
 				if(! JTplz.getText().isEmpty() && JTplz.getText() != null) {
 					JTplz.setBackground(Color.white);
 					JTplzstatus = true;
 				}else {
+					/*Hintergrund des TextFields wird gelb*/
 					JTplz.setBackground(Color.yellow);
 					JTplz.setOpaque(true);
 				}
 				
+				/*Es wird geprpft, ob der Ort definiert wurde*/
 				if(! JTort.getText().isEmpty() && JTort.getText() != null) {
 					JTort.setBackground(Color.white);
 					JTortstatus = true;
 				}else {
+					/*Hintergrund des TextFields wird gelb*/
 					JTort.setBackground(Color.yellow);
 					JTort.setOpaque(true);
 				}
 				
+				/*Es wird geprpft, ob das Land definiert wurde*/
 				if(! JTland.getText().isEmpty() && JTland.getText() != null) {
 					JTland.setBackground(Color.white);
 					JTlandstatus = true;
 				}else {
+					/*Hintergrund des TextFields wird gelb*/
 					JTland.setBackground(Color.yellow);
 					JTland.setOpaque(true);
 				}
 				
+				/*Alle Status müssen True sein*/
 				if(JTvornamestatus == true && JTnachnamestatus == true && JTstrassestatus == true && JTplzstatus == true && JTortstatus == true && JTlandstatus == true) {
+					/*Frame wird unsichtbar*/
 					setVisible(false);
+					/*Frame für das GUI5 wird erstellt*/
 					frameGUI5 = new GUI5(frameGUI4);
+					/*Frame für das GUI5 wird sichtbar*/
 					frameGUI5.setVisible(true);
 					
 				}
@@ -276,10 +311,16 @@ public class GUI4 extends JFrame{
 			}
 		});
 		
+		/**
+		 * Onclick-Action für Zurück:
+		 * Wechsel von der aktuellen Frame auf die vorherige Frame von GUI3
+		 */
 		JLzurück.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				/*Frame wird unsichtbar*/
 				setVisible(false);
+				/*Frame für das GUI3 wird sichtbar*/
 				GUI2.frameGUI3.setVisible(true);
 			}
 		});
--- src/GUI5.java
+++ src/GUI5.java
@@ -31,118 +31,142 @@ import javax.swing.JSeparator;
 
 public class GUI5 extends JFrame{
 	
+	/*Initialisierung der Frame für das GUI5*/
 	static GUI5 frameGUI5;
+	/*Farbe für Orange*/
 	Color myOrange = new Color(255, 127, 000);
 
 	public GUI5(GUI4 frameGUI4) {
 		
+		/*Einstellungen der Frame*/
 		setTitle("Rechnung");
 		setSize(450, 630);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		getContentPane().setLayout(null);
 		
+		/*globales Panel für die Frame*/
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 434, 591);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
+		/*Label für die Anzeige der Anrede*/
 		JLabel JLanrede = new JLabel(GUI3.frameGUI4.JCBanrede.getSelectedItem().toString());
 		JLanrede.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLanrede.setBounds(20, 11, 404, 24);
 		panel.add(JLanrede);
 		
+		/*Label für die Anzeige des Vor- und Nachnamens*/
 		JLabel JLname = new JLabel(GUI3.frameGUI4.JTvorname.getText()+" "+GUI3.frameGUI4.JTnachname.getText());
 		JLname.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLname.setBounds(20, 36, 404, 24);
 		panel.add(JLname);
 		
+		/*Label für die Anzeige der Strasse*/
 		JLabel JLstrasse = new JLabel(GUI3.frameGUI4.JTstrasse.getText());
 		JLstrasse.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLstrasse.setBounds(20, 61, 404, 24);
 		panel.add(JLstrasse);
 		
+		/*Label für die Anzeige der PLZ und des Orts*/
 		JLabel JLort = new JLabel(GUI3.frameGUI4.JTplz.getText()+" "+GUI3.frameGUI4.JTort.getText());
 		JLort.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLort.setBounds(20, 86, 404, 24);
 		panel.add(JLort);
 		
+		/*Trennbalken*/
 		JSeparator separator1 = new JSeparator();
 		separator1.setForeground(Color.BLACK);
 		separator1.setBounds(20, 121, 404, 10);
 		panel.add(separator1);
 		
+		/*Label für die Anzeige des Hinfluges*/
 		JLabel JLtitlehinflug = new JLabel("Von "+GUI1.abflughafen+" nach "+GUI1.zielflughafen);
 		JLtitlehinflug.setForeground(myOrange);
 		JLtitlehinflug.setFont(new Font("Tahoma", Font.BOLD, 13));
 		JLtitlehinflug.setBounds(20, 142, 404, 24);
 		panel.add(JLtitlehinflug);
 		
+		/*Label für die Anzeige der Abflugdaten*/
 		JLabel JLabflug1 = new JLabel("Abflug: "+GUI1.JThinflugdatum.getText()+" / "+GUI2.abflug1);
 		JLabflug1.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLabflug1.setBounds(20, 177, 404, 24);
 		panel.add(JLabflug1);
 		
+		/*Label für die Anzeige der Ankunftsdaten*/
 		JLabel JLankunft1 = new JLabel("Ankunft: "+GUI1.JThinflugdatum.getText()+" / "+GUI2.ankunft1);
 		JLankunft1.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLankunft1.setBounds(20, 202, 404, 24);
 		panel.add(JLankunft1);
 		
+		/*Label für die Anzeige des Rückfluges*/
 		JLabel JLtitlerückflug = new JLabel("Von "+GUI1.zielflughafen+" nach "+GUI1.abflughafen);
 		JLtitlerückflug.setForeground(myOrange);
 		JLtitlerückflug.setFont(new Font("Tahoma", Font.BOLD, 13));
 		JLtitlerückflug.setBounds(20, 237, 404, 24);
 		panel.add(JLtitlerückflug);
 		
+		/*Label für die Anzeige der Abflugdaten*/
 		JLabel JLabflug2 = new JLabel("Abflug: "+GUI1.JTrückflugdatum.getText()+" / "+GUI2.abflug2);
 		JLabflug2.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLabflug2.setBounds(20, 272, 404, 24);
 		panel.add(JLabflug2);
 		
+		/*Label für die Anzeige der Ankunftsdaten*/
 		JLabel JLankunft2 = new JLabel("Ankunft: "+GUI1.JTrückflugdatum.getText()+" / "+GUI2.ankunft2);
 		JLankunft2.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		JLankunft2.setBounds(20, 297, 404, 24);
 		panel.add(JLankunft2);
 		
+		/*Wurde ein Hin- und Rücklfug gewählt?*/
 		if(GUI1.statushinflug == false) {
+			/*Teile für den Rückflug werden unsichtbar gemacht*/
 			JLtitlerückflug.setVisible(false);
 			JLabflug2.setVisible(false);
 			JLankunft2.setVisible(false);
 		}
 		
+		/*Trennbalken*/
 		JSeparator separator2 = new JSeparator();
 		separator2.setForeground(Color.BLACK);
 		separator2.setBounds(20, 342, 404, 10);
 		panel.add(separator2);
 		
+		/*Label für die Anzeige der Anzahl Passagiere*/
 		JLabel JLtitlekosten = new JLabel("Kosten für "+(GUI1.count1+GUI1.count2+GUI1.count3)+" Person/en:");
 		JLtitlekosten.setForeground(Color.BLACK);
 		JLtitlekosten.setFont(new Font("Tahoma", Font.BOLD, 13));
 		JLtitlekosten.setBounds(20, 363, 404, 24);
 		panel.add(JLtitlekosten);
 		
+		/*Label für die Beschriftung der Kosten für die Flugstrecke*/
 		JLabel JLkosten1 = new JLabel("Kosten für Flugstrecke");
 		JLkosten1.setForeground(myOrange);
 		JLkosten1.setFont(new Font("Tahoma", Font.BOLD, 13));
 		JLkosten1.setBounds(20, 398, 181, 24);
 		panel.add(JLkosten1);
 		
+		/*Label für die Beschriftung der Kosten für die Sitzreservation*/
 		JLabel JLkosten2 = new JLabel("Sitzplatz");
 		JLkosten2.setForeground(myOrange);
 		JLkosten2.setFont(new Font("Tahoma", Font.BOLD, 13));
 		JLkosten2.setBounds(20, 425, 181, 24);
 		panel.add(JLkosten2);
 		
+		/*Label für Pfeil*/
 		JLabel JLarrow2 = new JLabel(">>>");
 		JLarrow2.setFont(new Font("Tahoma", Font.BOLD, 13));
 		JLarrow2.setBounds(229, 425, 39, 24);
 		panel.add(JLarrow2);
 		
+		/*Label für Pfeil*/
 		JLabel JLarrow1 = new JLabel(">>>");
 		JLarrow1.setFont(new Font("Tahoma", Font.BOLD, 13));
 		JLarrow1.setBounds(229, 398, 39, 24);
 		panel.add(JLarrow1);
 		
+		/*Label für die Anzeige des Preises für die Flugstrecke*/
 		JLabel JLbetrag1 = new JLabel(GUI2.price+" CHF");
 		JLbetrag1.setHorizontalAlignment(SwingConstants.RIGHT);
 		JLbetrag1.setForeground(Color.BLACK);
@@ -150,6 +174,7 @@ public class GUI5 extends JFrame{
 		JLbetrag1.setBounds(294, 398, 103, 24);
 		panel.add(JLbetrag1);
 		
+		/*Label für die Anzeige des Preises für die Sitzreservation*/
 		JLabel JLbetrag2 = new JLabel(GUI3.betrag+" CHF");
 		JLbetrag2.setHorizontalAlignment(SwingConstants.RIGHT);
 		JLbetrag2.setForeground(Color.BLACK);
@@ -157,11 +182,13 @@ public class GUI5 extends JFrame{
 		JLbetrag2.setBounds(294, 425, 103, 24);
 		panel.add(JLbetrag2);
 		
+		/*Trennbalken*/
 		JSeparator separator3 = new JSeparator();
 		separator3.setForeground(Color.BLACK);
 		separator3.setBounds(292, 460, 118, 10);
 		panel.add(separator3);
 		
+		/*Label für die Anzeige der totalen Kosten*/
 		JLabel JLtotal = new JLabel((GUI2.price+GUI3.betrag)+" CHF");
 		JLtotal.setHorizontalAlignment(SwingConstants.RIGHT);
 		JLtotal.setForeground(Color.BLACK);
@@ -169,6 +196,7 @@ public class GUI5 extends JFrame{
 		JLtotal.setBounds(294, 475, 103, 24);
 		panel.add(JLtotal);
 		
+		/*Button Drucken für das Abschliessen des Programmes*/
 		JLabel JLdrucken = new JLabel("Drucken >");
 		JLdrucken.setForeground(Color.WHITE);
 		JLdrucken.setBackground(myOrange);
@@ -178,6 +206,7 @@ public class GUI5 extends JFrame{
 		JLdrucken.setBounds(20, 527, 181, 53);
 		panel.add(JLdrucken);
 		
+		/*Button Abbrechen für das Abbrechen des Programms*/
 		JLabel JLabbrechen = new JLabel("Abbrechen <");
 		JLabbrechen.setHorizontalAlignment(SwingConstants.CENTER);
 		JLabbrechen.setForeground(Color.BLACK);
@@ -187,6 +216,7 @@ public class GUI5 extends JFrame{
 		JLabbrechen.setBounds(229, 527, 181, 53);
 		panel.add(JLabbrechen);
 		
+		/*Button Zurück für das Bearbeiten der persönlichen Daten auf der Frame von GUI4*/
 		JLabel JLzurück1 = new JLabel("Zurück <");
 		JLzurück1.setOpaque(true);
 		JLzurück1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -196,6 +226,7 @@ public class GUI5 extends JFrame{
 		JLzurück1.setBounds(321, 46, 89, 36);
 		panel.add(JLzurück1);
 		
+		/*Button Zurück für das Bearbeiten der Flugstrecke auf der Frame vom GUI2*/
 		JLabel JLzurück2 = new JLabel("Zurück <");
 		JLzurück2.setOpaque(true);
 		JLzurück2.setHorizontalAlignment(SwingConstants.CENTER);
@@ -205,33 +236,57 @@ public class GUI5 extends JFrame{
 		JLzurück2.setBounds(321, 183, 89, 36);
 		panel.add(JLzurück2);
 		
+		/**
+		 * Onclick-Action für Zurück1:
+		 * Es wird auf die Frame vom GUI4 gewechselt.
+		 */
 		JLzurück1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				/*Frame wird unsichtbar*/
 				setVisible(false);
+				/*Frame von GUI4 wird sichbar*/
 				GUI3.frameGUI4.setVisible(true);
 			}
 		});
 		
+		/**
+		 * Onclick-Action für Zurück1:
+		 * Es wird auf die Frame vom GUI1 gewechselt.
+		 */
 		JLzurück2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				/*Frame wird unsichtbar*/
 				setVisible(false);
+				/*Frame von GUI1 wird sichbar*/
 				GUI1.frame.setVisible(true);
 			}
 		});
 		
+		/**
+		 * Onclick-Action für Drucken:
+		 * Es wird nicht effektiv gedruckt, es kommt trotzdem eine Bestätigung.
+		 * Danach wird das Programm geschlossen.
+		 */
 		JLdrucken.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				 JOptionPane.showMessageDialog(frameGUI5,"Dokument wurde erfolgreich gedruckt (x)");
+				/*Dialog für die Bestätigung des Druckens*/
+				JOptionPane.showMessageDialog(frameGUI5,"Dokument wurde erfolgreich gedruckt");
+				/*Programm wird geschlossen*/
 				 dispose();
 			}
 		});
 		
+		/**
+		 * Onclick-Action für Abbrechen:
+		 * Programm wird geschlossen.
+		 */
 		JLabbrechen.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				/*Programm wird geschlossen*/
 				dispose();
 			}
 		});
